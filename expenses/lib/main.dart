fa143f3d92456c100d4d8866d4ee567c828817f3
fercguerra <fercguerra06@gmail.com>
2024-03-09 17:03:54 -0300

    Versão Inicial

---
+++ expenses/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ExpensesApp());
+  
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Versão Inicial'),
+    );
+  }
+}
