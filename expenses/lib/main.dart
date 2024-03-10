e5f2f0c04bca2e8288617f1725ddc1f769bdc944
fercguerra <fercguerra06@gmail.com>
2024-03-10 19:26:29 -0300

    Aula 108

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -19,6 +19,15 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: (() {
+              
+            }),),
+            
+          
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -35,6 +44,13 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (() {
+          
+        }),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
