445ecd6c8d337548807b251e8350475c13552b44
fercguerra <fercguerra06@gmail.com>
2024-03-12 14:16:40 -0300

    Fonts

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -14,6 +14,7 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
       theme: tema.copyWith(
         colorScheme: tema.colorScheme.copyWith(
